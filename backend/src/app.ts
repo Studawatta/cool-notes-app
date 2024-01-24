1afd2c4dbdb6e7660cd0cb860017cae45582ad78
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-01-24 10:02:27 +0530

    create note, get notes, get note api routes completed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,25 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import noteRoutes from './routes/notes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world!');
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use('/api/notes', noteRoutes);
+
+app.use((req, res, next) => {
+  next(Error('Endpoint not found!'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  let errorMessage = 'An unknown error occurred';
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
