a203532c5bed92b84767952c2349b90eab5f6473
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-01-28 14:47:18 +0530

    update note and delete note api routes compeleted

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -29,7 +29,7 @@ export const getNote: RequestHandler = async (req, res, next) => {
 };
 
 interface CreateNoteBody {
-  title: string;
+  title?: string;
   text?: string;
 }
 
@@ -50,6 +50,61 @@ export const createNote: RequestHandler<
       text: text,
     });
     res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface UpdateNoteParams {
+  noteId: string;
+}
+
+interface UpdateNoteBody {
+  title?: string;
+  text?: string;
+}
+export const updateNote: RequestHandler<
+  UpdateNoteParams,
+  unknown,
+  UpdateNoteBody,
+  undefined
+> = async (req, res, next) => {
+  const noteId = req.params.noteId;
+  const newTitle = req.body.title;
+  const newText = req.body.text;
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid note id');
+    }
+    if (!newTitle) {
+      throw createHttpError('400', 'Note must have a title');
+    }
+    const note = await NoteModel.findById(noteId).exec();
+    if (!note) {
+      throw createHttpError(404, 'Note not found!');
+    }
+
+    note.title = newTitle;
+    note.text = newText;
+
+    const updatedNote = await note.save();
+
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+  const noteId = req.params.noteId;
+
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid note id');
+    }
+
+    await NoteModel.findByIdAndDelete(noteId);
+    res.sendStatus(204);
   } catch (error) {
     next(error);
   }
